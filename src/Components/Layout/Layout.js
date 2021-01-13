666b61afe1f5f441eb6846831fe699019461c370
Lorena <lorenagabby4@gmail.com>
2021-01-12 19:36:31 -0500

    Adding a Sidedrawer Toggle Button

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -6,17 +6,22 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
     state={
-        showSideDrawer : true
+        showSideDrawer : false
     }
     sideDrawerCloseHandler = () => {
         this.setState({showSideDrawer:false});
+    }
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer};
+        });
     }
 
     render(){
         return(
 
         <Aux>
-        <Toolbar />
+        <Toolbar drawerToggleclicked = {this.sideDrawerToggleHandler}/>
         <SideDrawer 
             open={this.state.showSideDrawer} 
             closed={this.sideDrawerCloseHandler}/>
--- src/Components/Navigation/Toolbar/Toolbar.js
+++ src/Components/Navigation/Toolbar/Toolbar.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => (
     <header className={Classes.Toolbar}>
-        <div>MENU</div>
+        <DrawerToggle clicked= {props.drawerToggleclicked}/>
         <Logo  height='80%'/>
         <nav className={Classes.DesktopOnly}>
             <NavigationItems />
